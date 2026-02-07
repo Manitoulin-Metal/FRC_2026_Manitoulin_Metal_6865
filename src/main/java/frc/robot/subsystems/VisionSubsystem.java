a32d19fc054cc776e819f8bf309df05c78db7f53
6865-Manitoulin Metal <6865mssrobotics@gmail.com>
2026-02-07 08:08:04 -0500

    added Drive to AprilTag command

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -1,6 +1,31 @@
-// This is being used by Team 6865, Manitoulin Metal
-// This was created by Team 6865, Manitoulin Metal
-
 package frc.robot.subsystems;
 
-public class VisionSubsystem {}
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.vision.*;
+
+public class VisionSubsystem extends SubsystemBase {
+
+  private final VisionIO io;
+  private final VisionIOInputsAutoLogged inputs = new VisionIOInputsAutoLogged();
+
+  public VisionSubsystem(VisionIO io) {
+    this.io = io;
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+  }
+
+  public boolean hasTag(int id) {
+    return inputs.latestTargetObservation != null;
+  }
+
+  public double getTX() {
+    return inputs.latestTargetObservation.tx().getDegrees();
+  }
+
+  public double getTY() {
+    return inputs.latestTargetObservation.ty().getDegrees();
+  }
+}
