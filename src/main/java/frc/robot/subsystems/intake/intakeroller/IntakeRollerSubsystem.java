53fa705414d652ad16f332390016913ee5c397f7
hellorandomppl2-max <hellorandomppl2@gmail.com>
2026-02-10 11:26:45 -0800

    Moved all subsystems to there own folder  and made the changes needed to the robot code

--- src/main/java/frc/robot/subsystems/intake/intakeroller/IntakeRollerSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/intakeroller/IntakeRollerSubsystem.java
@@ -1,8 +1,8 @@
 // This is being used by Team 6865, Manitoulin Metal
 // This was created by Team 6865, Manitoulin Metal
 
-package frc.robot.subsystems;
+package frc.robot.subsystems.intake.intakeroller;
 
-public class KickerSubsystem {
+public class IntakeRollerSubsystem {
     
 }
--- src/main/java/frc/robot/subsystems/kicker/KickerSubsystem.java
+++ src/main/java/frc/robot/subsystems/kicker/KickerSubsystem.java
@@ -1,8 +1,8 @@
 // This is being used by Team 6865, Manitoulin Metal
 // This was created by Team 6865, Manitoulin Metal
 
-package frc.robot.subsystems;
+package frc.robot.subsystems.kicker;
 
-public class IntakeRollerSubsystem {
+public class KickerSubsystem {
     
 }
