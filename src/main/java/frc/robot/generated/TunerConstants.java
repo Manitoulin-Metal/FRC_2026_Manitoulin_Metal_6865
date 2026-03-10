8a1295d60a256615e4711316507fa5cc891c77e1
Pengu1n374 <thom5369@rscloud.ca>
2026-03-10 19:29:40 -0400

    TunerConstants.java had wrong ints for kCANivore & kCANdle

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -108,11 +108,11 @@ public class TunerConstants {
   private static final boolean kInvertRightSide = true;
 
   private static final int kPigeonId = 2;
-  private static final int kCANivore = 3;
+  private static final int kCANivore = 4;
   private static final int ThriftyNova1 = 55;
   private static final int ThriftyNova2 = 56;
   private static final int kShooterMoter = 61;
-  private static final int kCANDle = 4;
+  private static final int kCANDle = 3;
 
   // These are only used for simulation
 
