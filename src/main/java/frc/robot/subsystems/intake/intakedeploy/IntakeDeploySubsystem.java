a29cc501f91b4ff0c1f0585dec4c6b0f17177975
Pengu1n374 <thom5369@rscloud.ca>
2026-03-17 17:04:30 -0400

    March 17th Update to Code

--- src/main/java/frc/robot/subsystems/intake/intakedeploy/IntakeDeploySubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/intakedeploy/IntakeDeploySubsystem.java
@@ -3,82 +3,91 @@
 
 package frc.robot.subsystems.intake.intakedeploy;
 
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.jni.CANSparkJNI;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import org.littletonrobotics.junction.Logger;
 
 @SuppressWarnings("removal")
 public class IntakeDeploySubsystem extends SubsystemBase {
   // Initialize the motor (Flex/MAX are setup the same way)
-
   SparkMax intakeDeploy = new SparkMax(59, MotorType.kBrushless);
+
+  private final RelativeEncoder encoder;
+  private final SparkMaxPIDController pidController;
 
   /** Creates a new Subsystem. */
   public IntakeDeploySubsystem() {
+    intakeDeploy.restoreFactoryDefaults();
+    intakeDeploy.setInverted(true);
+    intakeDeploy.setIdleMode(IdleMode.kBrake);
 
-    // lc.setRangingMode.LaserCan.RangingMode.SHORT lc; setRegionOfInterest(new
-    // LaserCan.RegionOfInterest(8, 8, 16, 16));
-    // lc.setTimingBudget(LaserCan.TimingBudget.TIMING_BUDGET_33MS ); lc.setRegionOfInterest(new
-    // LaserCan.RegionOfInterest(8, 8, 16, 16));
-    // lc.setTimingBudget(LaserCan.TimingBudget.TIMING_BUDGET_33MS getLaserMeasurement() {
-    // LaserCan.Measurement measurement = lc.getMeasurement();
-    // if (measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT)
-    // {
-    // return measurement.distance_mm;);
+    // PID setup
+    encoder = intakeDeploy.getEncoder();
 
+    encoder.setPositionConversionFactor(1.0); // Assume 1:1, tune gearing
+
+    encoder.setVelocityConversionFactor(1.0 / 60.0); // RPS
+
+    pidController = intakeDeploy.getPIDController();
+    pidController.setP(Constants.IntakeDeploy.kP);
+    pidController.setI(Constants.IntakeDeploy.kI);
+    pidController.setD(Constants.IntakeDeploy.kD);
+    pidController.setFF(Constants.IntakeDeploy.kFF);
+    pidController.setFeedbackDevice(encoder);
   }
 
-  {
-    SparkMaxConfig config3 = new SparkMaxConfig();
-    config3.inverted(true).idleMode(IdleMode.kBrake);
-
-    // Apply configs - reset old parameters, and persist through power-cycles.
-    intakeDeploy.configure(
-        config3, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
-    {
-    }
+  public void deploy() {
+    pidController.setReference(Constants.IntakeDeploy.DEPLOY_SETPOINT_ROT, ControlType.kPosition);
   }
 
-  /**
-   * Sets motor controllers to run-to-pos based off distance
-   *
-   * @return a command
-   */
-  public final Command IntakeDeployCommand(double speed) {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return run(
-        () -> {
-          runIntakeDeploy(speed);
-        });
+  public void stow() {
+    pidController.setReference(Constants.IntakeDeploy.STOW_SETPOINT_ROT, ControlType.kPosition);
   }
 
+  public boolean atDeployPosition() {
+    return Math.abs(encoder.getPosition() - Constants.IntakeDeploy.DEPLOY_SETPOINT_ROT)
+        <= Constants.IntakeDeploy.POSITION_TOLERANCE_ROT;
+  }
+
+  public boolean atStowPosition() {
+    return Math.abs(encoder.getPosition() - Constants.IntakeDeploy.STOW_SETPOINT_ROT)
+        <= Constants.IntakeDeploy.POSITION_TOLERANCE_ROT;
+  }
+
+  public Command deployCommand() {
+    return Commands.runOnce(this::deploy).andThen(Commands.waitUntil(this::atDeployPosition));
+  }
+
+  public Command stowCommand() {
+    return Commands.runOnce(this::stow).andThen(Commands.waitUntil(this::atStowPosition));
+  }
+
+  // Legacy speed control
+  @Deprecated
   public void runIntakeDeploy(double speed) {
     intakeDeploy.set(speed);
   }
 
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean IntakeDeployCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
-  }
-
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    Logger.recordOutput("IntakeDeploy/PositionRot", encoder.getPosition());
+    Logger.recordOutput("IntakeDeploy/VelocityRPS", encoder.getVelocity());
   }
 
   @Override
   public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
+    // Simulation
   }
 }
