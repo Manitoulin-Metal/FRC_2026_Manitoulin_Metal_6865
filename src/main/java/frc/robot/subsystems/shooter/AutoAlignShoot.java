4e18f133fcb07150502aee8558aaa64ea96cf244
Pengu1n374 <thom5369@rscloud.ca>
2026-03-29 11:26:24 -0400

    speed finally fixed??

--- src/main/java/frc/robot/subsystems/shooter/AutoAlignShoot.java
+++ src/main/java/frc/robot/subsystems/shooter/AutoAlignShoot.java
@@ -1,1 +1,14 @@
-package frc.robot.subsystems.shooter;
+ /* package frc.robot.subsystems.shooter;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.Constants;
+
+public class AutoAlignShoot extends Command {
+
+  protected double getTargetRPS(double distanceMeters) {
+    // Use dynamic shooter RPS based on distance to target
+    return Constants.SHOOTER_TARGET_RPS_BY_DISTANCE[0]; // Placeholder: use first breakpoint for now
+  }
+}
+  */
