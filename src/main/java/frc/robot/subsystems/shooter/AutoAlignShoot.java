3360fe6512fdadee094197a14368a2e34c482c08
Pengu1n374 <thom5369@rscloud.ca>
2026-03-29 10:02:05 -0400

    fixed auto shoot&climb

--- src/main/java/frc/robot/subsystems/shooter/AutoAlignShoot.java
+++ src/main/java/frc/robot/subsystems/shooter/AutoAlignShoot.java
@@ -1,12 +1,1 @@
 package frc.robot.subsystems.shooter;
-
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.networktables.DoubleEntry;
-import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.Constants;
-import frc.robot.generated.TunerConstants;
-import frc.robot.subsystems.shooter.ShooterSubsystem;
-import frc.robot.subsystems.drive.Drive;
-import frc.robot.subsystems.vision.VisionSubsystem;
