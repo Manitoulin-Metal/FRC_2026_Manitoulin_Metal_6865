2843f7d2df202012c675dd20ec3c9b5da6533c60
mrgargall <argalla@rainbowschools.ca>
2026-04-09 19:22:19 -0400

    Add CANdle debug to dashboard

--- src/main/java/frc/robot/subsystems/LEDMinimal.java
+++ src/main/java/frc/robot/subsystems/LEDMinimal.java
@@ -1,8 +1,10 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.controls.SolidColor;
 import com.ctre.phoenix6.hardware.CANdle;
 import com.ctre.phoenix6.signals.RGBWColor;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LEDMinimal extends SubsystemBase {
@@ -10,14 +12,18 @@ public class LEDMinimal extends SubsystemBase {
     private final CANdle candle = new CANdle(4, "DriveCanivore");
 
     private final SolidColor blueRequest = new SolidColor(0, 7).withColor(new RGBWColor(0, 0, 255));
+    private StatusCode lastSetControlStatus = StatusCode.StatusCodeNotInitialized;
 
     public LEDMinimal() {
         candle.clearAllAnimations();
-        candle.setControl(blueRequest);
+        lastSetControlStatus = candle.setControl(blueRequest);
+        SmartDashboard.putString("LEDMinimal/CANdleSetControl", lastSetControlStatus.toString());
     }
 
     @Override
     public void periodic() {
-        candle.setControl(blueRequest);
+        lastSetControlStatus = candle.setControl(blueRequest);
+        SmartDashboard.putString("LEDMinimal/CANdleSetControl", lastSetControlStatus.toString());
+        SmartDashboard.putBoolean("LEDMinimal/CANdleSetControlOK", lastSetControlStatus.isOK());
     }
 }
