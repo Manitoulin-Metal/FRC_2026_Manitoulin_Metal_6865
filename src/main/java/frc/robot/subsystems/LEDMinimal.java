bb0d64f90291f698221ed7ac49a487af4d31857e
6865-Manitoulin Metal <6865mssrobotics@gmail.com>
2026-04-09 19:10:42 -0400

    intake toggle working

--- src/main/java/frc/robot/subsystems/LEDMinimal.java
+++ src/main/java/frc/robot/subsystems/LEDMinimal.java
@@ -6,18 +6,18 @@ import com.ctre.phoenix6.signals.RGBWColor;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LEDMinimal extends SubsystemBase {
-    @SuppressWarnings({ "deprecated", "removal" })
-    private final CANdle candle = new CANdle(4, "DriveCanivore");
+  @SuppressWarnings({"deprecated", "removal"})
+  private final CANdle candle = new CANdle(4, "DriveCanivore");
 
-    private final SolidColor blueRequest = new SolidColor(0, 7).withColor(new RGBWColor(0, 0, 255));
+  private final SolidColor blueRequest = new SolidColor(0, 7).withColor(new RGBWColor(0, 0, 255));
 
-    public LEDMinimal() {
-        candle.clearAllAnimations();
-        candle.setControl(blueRequest);
-    }
+  public LEDMinimal() {
+    candle.clearAllAnimations();
+    candle.setControl(blueRequest);
+  }
 
-    @Override
-    public void periodic() {
-        candle.setControl(blueRequest);
-    }
+  @Override
+  public void periodic() {
+    candle.setControl(blueRequest);
+  }
 }
