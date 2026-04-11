420260928f29a388ee6a989ade7f7bb26bb9ae71
Pengu1n374 <thom5369@rscloud.ca>
2026-04-11 17:05:19 -0400

    StdDev (Linear and Angular) values changed to Team 1360's recommandations

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -34,8 +34,8 @@ public class VisionConstants {
 
   // Standard deviation baselines, for 1 meter distance and 1 tag
   // (Adjusted automatically based on distance and # of tags)
-  public static double linearStdDevBaseline = 0.02; // Meters
-  public static double angularStdDevBaseline = 0.06; // Radians
+  public static double linearStdDevBaseline = 1.0; // Meters
+  public static double angularStdDevBaseline = 2.0; // Radians
 
   // Standard deviation multipliers for each camera
   // (Adjust to trust some cameras more than others)
