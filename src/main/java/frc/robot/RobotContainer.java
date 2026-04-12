3345d076d4aca25ad017d87d97ab9948e32eb9da
Pengu1n374 <thom5369@rscloud.ca>
2026-04-12 10:22:56 -0400

    Very Minor Notes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -184,7 +184,8 @@ public class RobotContainer {
         "Drive SysId (Dynamic Forward)", drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
     autoChooser.addOption(
         "Drive SysId (Dynamic Reverse)", drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
-  
+//  \\ ================================================= //
+ 
     configureButtonBindings();
 
     // CameraServer.startAutomaticCapture(0);
