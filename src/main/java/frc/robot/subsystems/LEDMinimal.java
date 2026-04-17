1a5497ddcacedc4dea0283ec41b784b25ac64911
Pengu1n374 <thom5369@rscloud.ca>
2026-04-17 11:34:19 -0400

    Added Full Comments On LEDMinimal

--- src/main/java/frc/robot/subsystems/LEDMinimal.java
+++ src/main/java/frc/robot/subsystems/LEDMinimal.java
@@ -21,21 +21,26 @@ public class LEDMinimal extends SubsystemBase {
   // Changes LEDs to a Solid Blue Colour
   private final SolidColor blueRequest =
       new SolidColor(0, 7)
-          .withLEDStartIndex(0)
-          .withLEDEndIndex(7)
+          .withLEDStartIndex(0) // The First LED INDEX NUMBER
+          .withLEDEndIndex(7) // The Last LED INDEX NUMBER
           .withColor(new RGBWColor(0, 0, 255)); // Pure Blue
-          
+
   // These are Variables that state what status the robot is in
   private StatusCode lastConfigStatus = StatusCode.StatusCodeNotInitialized;
   private StatusCode lastSetControlStatus = StatusCode.StatusCodeNotInitialized;
 
+  // States Everything the LEDMinimal is supposed to do
   public LEDMinimal() {
-    CANdleConfiguration config = new CANdleConfiguration();
+    CANdleConfiguration config = new CANdleConfiguration(); // Configures the CANdle
+
+    // Below Configures What The LEDs Are Supposed To Do
     config.withLED(
         new LEDConfigs()
             .withBrightnessScalar(1.0)
             .withStripType(StripTypeValue.RGB)
             .withLossOfSignalBehavior(LossOfSignalBehaviorValue.KeepRunning));
+
+    // Configures CANdle's Features
     config.withCANdleFeatures(
         new CANdleFeaturesConfigs()
             .withEnable5VRail(Enable5VRailValue.Enabled)
@@ -45,6 +50,8 @@ public class LEDMinimal extends SubsystemBase {
 
     candle.clearAllAnimations();
     lastSetControlStatus = candle.setControl(blueRequest);
+
+    // Logging on SmartDashboard
     SmartDashboard.putString("LEDMinimal/CANdleConfig", lastConfigStatus.toString());
     SmartDashboard.putBoolean("LEDMinimal/CANdleConfigOK", lastConfigStatus.isOK());
     SmartDashboard.putString("LEDMinimal/CANdleSetControl", lastSetControlStatus.toString());
@@ -54,6 +61,8 @@ public class LEDMinimal extends SubsystemBase {
   @Override
   public void periodic() {
     lastSetControlStatus = candle.setControl(blueRequest);
+
+    // These Logging Commands Constantly Run When Robot Is On
     SmartDashboard.putString("LEDMinimal/CANdleConfig", lastConfigStatus.toString());
     SmartDashboard.putBoolean("LEDMinimal/CANdleConfigOK", lastConfigStatus.isOK());
     SmartDashboard.putString("LEDMinimal/CANdleSetControl", lastSetControlStatus.toString());
